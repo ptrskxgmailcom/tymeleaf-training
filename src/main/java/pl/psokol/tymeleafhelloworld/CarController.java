23633066a59f1078c25fb69a20178aaabf3b4412
psokol <piotr.sokol@t-mobile.pl>
2021-11-02 23:49:22 +0100

    list iteration

--- src/main/java/pl/psokol/tymeleafhelloworld/CarController.java
+++ src/main/java/pl/psokol/tymeleafhelloworld/CarController.java
@@ -5,15 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.psokol.tymeleafhelloworld.model.Car;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public String getCar(Model model){
-        Car car = new Car("Syrenka", "FSM");
+        Car car1 = new Car("Syrenka", "FSM");
+        Car car2 = new Car("BMW", "i8");
+        Car car3 = new Car("Fiat", "126p");
+        Car car4 = new Car("Polonez Caro", "FSM");
+
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
 
         model.addAttribute("name", "Piotor");
-        model.addAttribute("car", car);
+        model.addAttribute("cars", cars);
         return "car";
 
     }
