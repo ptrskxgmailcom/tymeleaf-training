a4b38e250051747494182f190bc095d8fcb1f5db
psokol <piotr.sokol@t-mobile.pl>
2021-11-03 22:11:05 +0100

    add element

--- src/main/java/pl/psokol/tymeleafhelloworld/CarController.java
+++ src/main/java/pl/psokol/tymeleafhelloworld/CarController.java
@@ -3,28 +3,46 @@ package pl.psokol.tymeleafhelloworld;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.psokol.tymeleafhelloworld.model.Car;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String getCar(Model model){
+    private List<Car> cars;
+
+    public CarController() {
         Car car1 = new Car("Syrenka", "FSM");
         Car car2 = new Car("BMW", "i8");
         Car car3 = new Car("Fiat", "126p");
         Car car4 = new Car("Polonez Caro", "FSM");
 
-        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
-
-        model.addAttribute("name", "Piotor");
-        model.addAttribute("cars", cars);
-        return "car";
+        cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
 
     }
 
+    @GetMapping("/car")
+    public String getCar(Model model){
+
+        model.addAttribute("name", "Piotor");
+        model.addAttribute("cars", cars);
+        model.addAttribute("newCar", new Car());
+        return "car";
+    }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car){
+        cars.add(car);
+        return "redirect:/car";
+    }
 
 }
